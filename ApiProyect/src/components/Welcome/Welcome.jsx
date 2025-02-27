2c79bebbf7a8029f971f418857cd56fce7ca0e0c
aldafernandez <aldana.fernandez@outlook.com.ar>
2025-02-26 21:57:59 -0300

    .

--- ApiProyect/src/components/Welcome/Welcome.jsx
+++ ApiProyect/src/components/Welcome/Welcome.jsx
@@ -6,7 +6,7 @@ export function Welcome() {
     return(
         <div className="welcome">
             <div className='box-welcome'>
-                <h1>Bienvenidos a MobiApi</h1>
+                <h1 className='title font-title'>MobiApi</h1>
                 <p>El lugar donde encontrarás las mejores películas</p>
 
             </div>
--- ApiProyect/src/main.jsx
+++ ApiProyect/src/main.jsx
@@ -6,7 +6,7 @@ import { App } from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
 
-  <div className='flex center-center column '>
+  <div >
     <App />
   </div>,
 )
