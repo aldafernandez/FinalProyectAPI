15c569e9b70e3589f61fbdfd6f6d7cd7a6383e87
aldafernandez <aldana.fernandez@outlook.com.ar>
2025-03-03 23:30:12 -0300

    header

--- ApiProyect/src/Components/Header/Header.jsx
+++ ApiProyect/src/Components/Header/Header.jsx
@@ -1,10 +1,15 @@
 import './Header.css';
+import { FaSearch } from "react-icons/fa";
 
 export function Header() {
     return (
-        <header>
-            <nav>
-               <p>buscador</p>
+        <header>       
+            <nav className='header'>
+                <h1 className='font-honk'>Moblix</h1>
+                <div className='search-bar'>
+                    <FaSearch className='icon' color='#444' size={12} />
+                    <input className='input' type='text' placeholder='Buscar'/>  
+                </div>
             </nav>      
         </header>
     );
